956246abee3ba566b4263a782b934d8e3bc732da
Christoph Dworschak <dworschak@posteo.de>
2020-09-16 10:06:51 +0200

    Added ACLED terms acknowledgement to intro message

--- R/acled.api.internal.R
+++ R/acled.api.internal.R
@@ -1,7 +1,9 @@
 
 .onAttach <-
   function(libname, pkgname) {
-    packageStartupMessage("\nPlease cite as: \n")
-    packageStartupMessage(' Dworschak, Christoph. 2020. "Acled.api: Automated Retrieval of ACLED Conflict Event Data."')
-    packageStartupMessage(" R package version 1.0.1. <https://github.com/chris-dworschak/acled.api> \n")
+    packageStartupMessage("\nThe data must be cited as per ACLED attribution requirements. By using this package, you ")
+    packageStartupMessage("acknowledge that you have read ACLED's terms and conditions. \n")
+    packageStartupMessage("The package may be cited as:")
+    packageStartupMessage('Dworschak, Christoph. 2020. "Acled.api: Automated Retrieval of ACLED Conflict Event Data."')
+    packageStartupMessage("R package version 1.0.1. <https://github.com/chris-dworschak/acled.api/> \n")
   }
