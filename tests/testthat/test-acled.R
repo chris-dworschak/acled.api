c1faefc16e72c171fa723ecc8f1bd8e92536e965
Rob Williams <jayrobwilliams@gmail.com>
2021-02-26 18:19:40 -0500

    add inter filters

--- tests/testthat/test-acled.R
+++ tests/testthat/test-acled.R
@@ -1,3 +1,60 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("inter1 and inter2 work", {
+  expect_length(unique(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                                 access.key = Sys.getenv("ACCESS_KEY"),
+                                 start.date = "2020-11-01",
+                                 end.date = "2020-11-31",
+                                 inter1 = c(2, 3),
+                                 inter2 = 8)[, "interaction"]),
+                n=2)
+  expect_length(unique(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                                 access.key = Sys.getenv("ACCESS_KEY"),
+                                 start.date = "2020-11-01",
+                                 end.date = "2020-11-31",
+                                 inter1 = 1,
+                                 inter2 = c(6, 7))[, "interaction"]),
+                n=2)
+  })
+
+test_that("interaction works", {
+  expect_length(unique(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                                 access.key = Sys.getenv("ACCESS_KEY"),
+                                 start.date = "2020-11-01",
+                                 end.date = "2020-11-31",
+                                 interaction = c(18, 28, 38))[, "interaction"]),
+                n = 3)
+})
+
+test_that("inter* validation works", {
+  expect_error(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                         access.key = Sys.getenv("ACCESS_KEY"),
+                         start.date = "2020-11-01",
+                         end.date = "2020-11-31",
+                         inter1 = 2,
+                         interaction = 18),
+               regexp = "All actor 1 type codes must be present in both the 'inter1' and 'interaction' arguments")
+  expect_error(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                         access.key = Sys.getenv("ACCESS_KEY"),
+                         start.date = "2020-11-01",
+                         end.date = "2020-11-31",
+                         inter2 = 2,
+                         interaction = 18),
+               regexp = "All actor 2 type codes must be present in both the 'inter2' and 'interaction' arguments")
+  expect_error(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                         access.key = Sys.getenv("ACCESS_KEY"),
+                         start.date = "2020-11-01",
+                         end.date = "2020-11-31",
+                         inter1 = "not an inter code"),
+               regexp = "The argument 'inter1' requires a numeric value")
+  expect_error(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                         access.key = Sys.getenv("ACCESS_KEY"),
+                         start.date = "2020-11-01",
+                         end.date = "2020-11-31",
+                         inter2 = "not an inter code"),
+               regexp = "The argument 'inter2' requires a numeric value")
+  expect_error(acled.api(email.address = Sys.getenv("EMAIL_ADDRESS"),
+                         access.key = Sys.getenv("ACCESS_KEY"),
+                         start.date = "2020-11-01",
+                         end.date = "2020-11-31",
+                         interaction = "not an inter code"),
+               regexp = "The 'interaction' argument requires a numeric value")
 })
